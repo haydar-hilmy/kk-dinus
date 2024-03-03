0b7604744f161f76236bd5f46455b8fb041ef488
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2024-03-03 20:43:33 +0700

    first commit

---
+++ app/Views/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <script src="https://kit.fontawesome.com/a502a8bc22.js" crossorigin="anonymous"></script> -->
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script> -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter">
+    <!-- <link rel="shortcut icon" href="" type="image/x-icon"> -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <link rel="stylesheet" href="css/main.css">
+
+    <title>CMD-QE Laboratory – Computational Materials Design and Quantum Engineering</title>
+</head>
+
+<body>
+
+    <div class="page">
+
+        <header>
+            <div class="header">
+                <div class="header-left-section"></div>
+                <div class="header-right-section"></div>
+            </div>
+            <div class="slider"></div>
+        </header>
+
+        <div class="main">
+            <article>
+                <div class="primary">
+                    <!-- <h2>Welcome to Computational Materials Design and Quantum Engineering (CMD-QE) Lab at ITB!</h2> -->
+                    <!-- <p>Our research group utilizes and develops atomistic-based multi-scale materials simulation techniques (e.g. first-principles density functional theory calculations, ab-initio and classical molecular dynamics and kinetic Monte Carlo (kMC)) to study various physical and chemical properties of materials. We have broad interest in various topics related (but not limited) to: solid-state catalysts for various chemical reactions (e.g. direct methane-to-methanol conversion, oxygen reduction reaction (ORR), etc.); electronic and ionic conductivities in solid state materials and polymers; materials for hydrogen and direct-hydrazine fuel cells; materials for drug delivery; materials for gas sensors; and materials for energy conversion and storage (e.g. photovoltaic, Li/Na-ion batteries, Metal-air batteries, etc.). Our recent effort also include machine-learning and data-driven-based approach to accelerate the discovery of new functional materials. Beyond materials-related research, our group also interested in the theoretical study related to quantum technologies (e.g. quantum computing, quantum thermodynamics and quantum measurement).</p> -->
+                </div>
+
+                <div class="secondary">
+                    <div class="widget"></div>
+                    <aside></aside>
+                </div>
+
+            </article>
+        </div>
+
+        <footer>
+            <div class="footer-widget">
+                <div class="column-widget"></div>
+                <div class="column-widget"></div>
+                <div class="column-widget"></div>
+            </div>
+            <div class="footer-widget-socket">
+                <!-- <p>Copyright © 2024 CMD-QE Laboratory. Powered by WordPress. Theme: Spacious by ThemeGrill</p> -->
+            </div>
+        </footer>
+
+    </div>
+
+</body>
+
+</html>
