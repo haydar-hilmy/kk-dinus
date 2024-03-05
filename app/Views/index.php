2a2d3fec222d1c7663431f269d6c9eccb13a46af
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2024-03-05 16:55:21 +0700

    fix: navbar css, vendor

--- app/Views/index.php
+++ app/Views/index.php
@@ -53,27 +53,42 @@
                             <a href="#">News</a>
                         </li>
                         <li>
-                            <a href="#">Members</a>
-                            <ul>
-                                <li>Loremmember</li>
-                                <li>Loremmember</li>
-                                <li>Loremmember</li>
+                            <a href="#">
+                                Members
+                                <svg class="dropdown-icon" width="1rem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                    <path d="M98.9 184.7l1.8 2.1 136 156.5c4.6 5.3 11.5 8.6 19.2 8.6 7.7 0 14.6-3.4 19.2-8.6L411 187.1l2.3-2.6c1.7-2.5 2.7-5.5 2.7-8.7 0-8.7-7.4-15.8-16.6-15.8v0H112.6v0c-9.2 0-16.6 7.1-16.6 15.8 0 3.2 1.1 6.3 2.9 8.8z" />
+                                </svg>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <a>Loremmember</a>
+                                <a>Loremmember</a>
+                                <a>Loremmember</a>
                             </ul>
                         </li>
                         <li>
-                            <a href="#">Research</a>
-                            <ul>
-                                <li>loremresearch</li>
-                                <li>loremresearch</li>
-                                <li>loremresearch</li>
+                            <a href="#">
+                                Research
+                                <svg class="dropdown-icon" width="1rem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                    <path d="M98.9 184.7l1.8 2.1 136 156.5c4.6 5.3 11.5 8.6 19.2 8.6 7.7 0 14.6-3.4 19.2-8.6L411 187.1l2.3-2.6c1.7-2.5 2.7-5.5 2.7-8.7 0-8.7-7.4-15.8-16.6-15.8v0H112.6v0c-9.2 0-16.6 7.1-16.6 15.8 0 3.2 1.1 6.3 2.9 8.8z" />
+                                </svg>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <a>loremresearch</a>
+                                <a>loremresearch</a>
+                                <a>loremresearch</a>
                             </ul>
                         </li>
                         <li>
-                            <a href="#">Project</a>
-                            <ul>
-                                <li>loremproject</li>
-                                <li>loremproject</li>
-                                <li>loremproject</li>
+                            <a href="#">
+                                Project
+                                <svg class="dropdown-icon" width="1rem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                    <path d="M98.9 184.7l1.8 2.1 136 156.5c4.6 5.3 11.5 8.6 19.2 8.6 7.7 0 14.6-3.4 19.2-8.6L411 187.1l2.3-2.6c1.7-2.5 2.7-5.5 2.7-8.7 0-8.7-7.4-15.8-16.6-15.8v0H112.6v0c-9.2 0-16.6 7.1-16.6 15.8 0 3.2 1.1 6.3 2.9 8.8z" />
+                                </svg>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <a>loremproject</a>
+                                <a>loremproject</a>
+                                <a>loremproject</a>
                             </ul>
                         </li>
                         <li>
