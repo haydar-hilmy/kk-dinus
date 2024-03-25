bcb9bab16d1c4d0e8851baef704a6699fb141032
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2024-03-25 23:23:37 +0700

    fix: superadmin view

--- app/Views/admin/layouts/add-banner.php
+++ app/Views/admin/layouts/add-banner.php
@@ -2,32 +2,27 @@
 
 <?= $this->section('content') ?>
 
-
-
 <div>
     <h2>Add Banner</h2>
 
     <form action="" method="post">
-        <div class="input-wrap">
+        <div class="form-group mt-3">
             <label for="judul">Judul Banner</label>
-            <input type="text" id="judul" name="judul">
-            <h5>judul harus diisi</h5>
+            <input type="text" name="judul" class="form-control" id="judul" placeholder="Masukkan Judul">
         </div>
 
-        <div class="input-wrap">
-            <label for="deskripsi">Judul Banner</label>
-            <textarea name="deskripsi" id="deskripsi"></textarea>
-            <h5>judul harus diisi</h5>
-        </div>
-        
-        <div>
-            <label for="file">Choose a file</label>
-            <input type="file" id="file" name="foto">
-            <h5>judul harus diisi</h5>
+        <div class="form-group mt-3">
+            <label for="deskripsi">Deskripsi Banner</label>
+            <textarea class="form-control" name="deskripsi" id="deskripsi" rows="3" placeholder="Masukkan Deskripsi"></textarea>
         </div>
 
-        <div>
-            <button type="submit">Submit</button>
+        <div class="custom-file mt-3">
+            <input type="file" class="custom-file-input" id="customFile">
+            <label class="custom-file-label" name="file" for="customFile">Choose file</label>
+        </div>
+
+        <div class="form-group mt-3">
+            <button class="btn btn-primary" type="submit">Submit form</button>
         </div>
     </form>
 </div>
--- app/Views/admin/navbar-admin.php
+++ app/Views/admin/navbar-admin.php
@@ -3,35 +3,41 @@
         <button class="menu-primary-btn">MENU</button>
         <ul>
             <li>
-                <a href="/">Home</a>
-            </li>
-            <li>
-                <a href="#">
-                    KK Dinus
-                    <svg class="dropdown-icon" width="1rem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                        <path d="M98.9 184.7l1.8 2.1 136 156.5c4.6 5.3 11.5 8.6 19.2 8.6 7.7 0 14.6-3.4 19.2-8.6L411 187.1l2.3-2.6c1.7-2.5 2.7-5.5 2.7-8.7 0-8.7-7.4-15.8-16.6-15.8v0H112.6v0c-9.2 0-16.6 7.1-16.6 15.8 0 3.2 1.1 6.3 2.9 8.8z" />
-                    </svg>
+                <a href="<?= base_url('superadmin') ?>">
+                    <button type="button" class="btn btn-primary">
+                        Home
+                    </button>
                 </a>
-                <ul class="dropdown-menu">
-                    <a href="#">Manage KK</a>
-                    <?php foreach ($kelompokKajian as $key => $kk): ?>
-                        <a href="#"><?= $kk->fullName ?></a>
-                    <?php endforeach ?>
-                </ul>
             </li>
             <li>
-                <a href="#">
-                    Layouts
-                    <svg class="dropdown-icon" width="1rem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                        <path d="M98.9 184.7l1.8 2.1 136 156.5c4.6 5.3 11.5 8.6 19.2 8.6 7.7 0 14.6-3.4 19.2-8.6L411 187.1l2.3-2.6c1.7-2.5 2.7-5.5 2.7-8.7 0-8.7-7.4-15.8-16.6-15.8v0H112.6v0c-9.2 0-16.6 7.1-16.6 15.8 0 3.2 1.1 6.3 2.9 8.8z" />
-                    </svg>
+                <div class="btn-group">
+                    <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        KK Dinus
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Manage KK</a>
+                        <?php foreach ($kelompokKajian as $key => $kk) : ?>
+                            <a class="dropdown-item" href="#"><?= $kk->fullName ?></a>
+                        <?php endforeach ?>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="btn-group">
+                    <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Layouts
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="<?= base_url('superadmin/banner') ?>">Banner</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="/logout">
+                    <button type="button" class="btn btn-danger">
+                        Logout
+                    </button>
                 </a>
-                <ul class="dropdown-menu">
-                    <a href="<?= base_url('superadmin/banner') ?>">Banner</a>
-                </ul>
-            </li>
-            <li>
-                <a href="/logout">Logout</a>
             </li>
         </ul>
     </div>
